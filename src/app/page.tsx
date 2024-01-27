687f409970a1a822861d2e00fc2ff6c0524f20b1
Caio <caiocpe@gmail.com>
2024-01-27 00:30:06 -0300

    a troca de cores no dropdown no momento está funcionando apenas no build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { readFile } from "@/utils";
 import { Info } from "@/models";
+import { IoMail } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import Dropdown from "@/components/Dropdown";
 
 export default async function Home() {
 	const info: Info = await readFile("./src/info.json");
@@ -7,24 +11,45 @@ export default async function Home() {
 
 	return (
 		<>
-			<header
-				className={"pb-10 bg-[url('./../../public/img/wallpaper.jpg')] bg-cover bg-[0%_20%]"}
-			>
-				<div className={"bg-zinc-800 w-fit p-3 rounded-br-lg"}>
+			<header className="pb-10 bg-[url('./../../public/img/wallpaper.jpg')] bg-cover bg-[0%_20%] flex justify-between">
+				<div className="bg-zinc-800 w-fit p-3 rounded-br-lg">
 					<img
 						src="img/perfil1.jpg"
 						alt="Foto de perfil"
-						className={"max-h-96 rounded-lg"}
+						className="max-h-96 rounded-lg"
 					/>
-					<div className={"flex-col text-white pt-2"}>
-						<p className={"font-bold text-center"}>
-							{info.fullName}
-						</p>
-						<p className={"font-bold text-center"}>
+					<div className="flex flex-col justify-center text-white pt-2">
+						<p className="font-bold text-center">{info.fullName}</p>
+						<p className="font-bold text-center">
 							{info.location.city}, {info.location.state}
 						</p>
 					</div>
 				</div>
+
+				<Dropdown optionalClass="text-white border-0 bg-blue-700 font-bold">
+					{info.emails.map((email) => (
+						<div
+							key={email}
+							className="inline-flex items-center gap-1"
+						>
+							<IoMail className="text-red-500" />
+							<li className="mb-3 last:mb-0">{email}</li>
+						</div>
+					))}
+					{info.phones.map(({ phone, whatsApp }) => (
+						<div
+							key={phone}
+							className="inline-flex items-center gap-1"
+						>
+							{whatsApp ? (
+								<IoLogoWhatsapp className="text-green-600" />
+							) : (
+								<FaPhone />
+							)}
+							<li className="mb-3 last:mb-0">{phone}</li>
+						</div>
+					))}
+				</Dropdown>
 			</header>
 		</>
 	);
---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,61 @@
+"use client";
+import {
+	HTMLAttributes,
+	PropsWithChildren,
+	forwardRef,
+	useEffect,
+	useRef,
+} from "react";
+
+interface DropdownProps extends PropsWithChildren, HTMLAttributes<HTMLElement> {
+	optionalClass?: string;
+}
+
+export default forwardRef<HTMLElement, DropdownProps>(
+	({ children, optionalClass }: DropdownProps, ref) => {
+		const ref2 = useRef<HTMLElement | null>(null);
+		const reference = ref || ref2;
+
+		useEffect(() => {
+			if (optionalClass) {
+				if (
+					typeof reference === "object" &&
+					reference.hasOwnProperty("current")
+				) {
+					reference.current?.addEventListener("click", () => {
+						optionalClass.split(" ").forEach((c) => {
+                            if(reference.current){
+                                console.log(`avalidando ${c}`)
+                                let res = reference.current.classList.contains(c)
+                                
+                                if(res){
+                                    console.log(`${c} existe`)
+                                    reference.current.classList.remove(c)
+                                    console.log(`${reference.current.className}`)
+                                }
+                                else{
+                                    console.log(`adicionarei ${c}`)
+                                    reference.current.classList.add(c)
+                                    console.log(`${reference.current.className}`)
+                                }
+                            }
+						});
+					});
+				}
+			}
+		}, [optionalClass]);
+
+		return (
+			<>
+				<details className="dropdown dropdown-end" open>
+					<summary className="m-1 btn" ref={reference}>
+						Contatos
+					</summary>
+					<ul className="p-3 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-fit font-bold underline underline-offset-2">
+						{children}
+					</ul>
+				</details>
+			</>
+		);
+	}
+);
