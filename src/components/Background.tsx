9ee30503a7d3e57e4fc7d4939a8079559ebdab1e
Caio <caiocpe@gmail.com>
2024-02-03 14:19:37 -0300

    renomeando imagens

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -23,12 +23,8 @@ export default function Background({
 		<>
 			<div
 				id="background"
-				className="bg-[url('./../../public/img/pexels-josh-sorenson-1714202-1000.jpg')] w-full h-full bg-cover bg-[60%_0%] relative"
+				className="bg-[url('./../../public/img/wallpaper1.jpg')] w-full h-full bg-cover bg-[60%_0%] relative"
 			>
-				{/* <div className="bg-[url('./../../public/img/pexels-danny-meneses-943096-1000.jpg')] w-full h-full bg-cover bg-[80%_0%]"> */}
-				{/* <div className="bg-[url('./../../public/img/pexels-luis-gomes-546819-1000.jpg')] w-full h-full bg-cover bg-[10%_0%]"> */}
-				{/* <div className="bg-[url('./../../public/img/pexels-pixabay-50711-1000.jpg')] w-full h-full bg-cover bg-[20%_0%]"> */}
-				{/* <div className="bg-[url('./../../public/img/technology-linkedin-background-lh8lf9sg6gog8ad0.jpg')] w-full h-full bg-cover bg-[0%_0%]"> */}
 				<div className="bg-black/80 w-full h-full">{children}</div>
 				<div className="absolute top-[100%] w-full flex justify-end pr-1">
                     <p
