48199bda779894b3a9238c8570bdb431f09ee8fa
Caio <caiocpe@gmail.com>
2024-02-06 19:26:52 -0300

    limitando a largura máxima da página

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -21,22 +21,24 @@ export default function Background({
 
 	return (
 		<>
-			<div
-				id="background"
-				className="bg-[url('./../../public/img/wallpaper1.jpg')] w-full h-full bg-cover bg-[60%_0%] relative"
-			>
-				<div className="bg-black/80 w-full h-full">{children}</div>
-				<div className="absolute top-[100%] w-full flex justify-end pr-1">
-                    <p
-                        className="text-zinc-700 text-sm w-fit cursor-pointer"
-                        onClick={() => {
-                            navigator.clipboard.writeText("");
-                            handleToast("Link copiado");
-                        }}
-                    >
-                        Photo by Josh Sorenson
-                    </p>
-                </div>
+			<div className="w-full h-full flex justify-center">
+				<div
+					id="background"
+					className="bg-[url('./../../public/img/wallpaper1.jpg')] bg-cover bg-[60%_0%] relative max-w-screen-xl"
+				>
+					<div className="bg-black/80 w-full h-full">{children}</div>
+					<div className="absolute top-[99%] w-full flex justify-end pr-1">
+						<p
+							className="text-zinc-700 text-sm w-fit cursor-pointer"
+							onClick={() => {
+								navigator.clipboard.writeText("");
+								handleToast("Link copiado");
+							}}
+						>
+							Photo by Josh Sorenson
+						</p>
+					</div>
+				</div>
 			</div>
 			{showToast && (
 				<>
